2819d6a5eae20c47979795327522fa2a4e4d87f9
Floern <dev@floern.com>
2017-08-18 22:54:16 +0200

    api exception details

--- src/main/java/com/floern/genericbot/frame/stackexchange/api/net/ApiException.java
+++ src/main/java/com/floern/genericbot/frame/stackexchange/api/net/ApiException.java
@@ -7,12 +7,38 @@ import com.floern.genericbot.frame.stackexchange.api.model.Container;
 
 public class ApiException extends Exception {
 
+	private int errorId;
+	private String errorName;
+	private String errorMessage;
+
 	public ApiException(Container container) {
 		this(container, null);
 	}
 
 	public ApiException(Container container, Throwable cause) {
 		super("API Error " + container.getErrorId() + " " + container.getErrorName() + ": " + container.getErrorMessage(), cause);
+		errorId = container.getErrorId();
+		errorName = container.getErrorName();
+		errorMessage = container.getErrorMessage();
+	}
+
+	public int getErrorId() {
+		return errorId;
+	}
+
+	public String getErrorName() {
+		return errorName;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public static int getErrorId(Exception e) {
+		if (e instanceof ApiException) {
+			return ((ApiException)e).getErrorId();
+		}
+		return 0;
 	}
 
 }
