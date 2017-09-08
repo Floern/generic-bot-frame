c0462d57cd8764c9725212035823c4c1e53e8f32
Floern <dev@floern.com>
2017-09-09 00:08:01 +0200

    condensed exception format

--- src/main/java/com/floern/genericbot/frame/utils/ChatPrinter.java
+++ src/main/java/com/floern/genericbot/frame/utils/ChatPrinter.java
@@ -3,9 +3,16 @@
  */
 package com.floern.genericbot.frame.utils;
 
+import com.google.common.collect.Lists;
+
 import java.util.Arrays;
+import java.util.List;
 
 public class ChatPrinter {
+
+
+	private static final List<String> INTERNAL_PACKAGE_PREFIXES =
+			Lists.newArrayList("org.apache.http.", "java.net.", "sun.security.ssl.");
 
 
 	public static String formatException(Throwable e) {
@@ -16,6 +23,26 @@ public class ChatPrinter {
 			Arrays.stream(e.getStackTrace()).limit(4).forEach(stackTraceElement -> {
 				sb.append(linePrefix).append(linePrefix).append(stackTraceElement.toString()).append('\n');
 			});
+			e = e.getCause();
+		}
+		return sb.toString();
+	}
+
+
+	public static String formatExceptionCondensed(Throwable e) {
+		StringBuilder sb = new StringBuilder(512);
+		String linePrefix = "    ";
+		while (e != null) {
+			sb.append(linePrefix).append(e.getClass().getName()).append(": ").append(e.getMessage()).append('\n');
+			String prevPackagePrefix = "~";
+			for (StackTraceElement stackTraceElement : e.getStackTrace()) {
+				if (stackTraceElement.getClassName().startsWith(prevPackagePrefix))
+					continue;
+				prevPackagePrefix = INTERNAL_PACKAGE_PREFIXES.stream()
+						.filter(prefix -> stackTraceElement.getClassName().startsWith(prefix))
+						.findAny().orElse(stackTraceElement.getClassName());
+				sb.append(linePrefix).append(linePrefix).append(stackTraceElement.toString()).append('\n');
+			}
 			e = e.getCause();
 		}
 		return sb.toString();
